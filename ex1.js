6a0465173e4a8157fa32c5ed9407acbcae444e21
gigii21 <128159936+gigii21@users.noreply.github.com>
2024-04-05 09:47:05 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,4 @@
+var Nome = "Giovanna";
+var Idade = "17";
+
+console.log(`Oi! Me chamo ${Nome} e tenho ${Idade} anos.`);
---
+++ ex3.js
@@ -0,0 +1,9 @@
+let usuario = 17
+
+ if(usuario >= 18){
+    var maioridade = "maior de idade"
+ }else {
+    var maioridade = "menor de idade"
+ }
+
+ console.log(`o usuario com ${usuario} anos é ${maioridade}.`) 
