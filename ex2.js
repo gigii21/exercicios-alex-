388cbef047546684b98e42fbab5a7b5dedb7a89c
gigii21 <128159936+gigii21@users.noreply.github.com>
2024-04-18 12:23:43 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,9 @@
+let n1 = 25;
+let n2 = 9;
+
+//console.log(n1 + n2, n1 - n2, n1 * n2, n1/n2)
+
+console.log(`a soma destes números é ${n1 + n2}`);
+console.log(`a sbtração destes números é ${n1 - n2}`);
+console.log(`a multiplicação destes números é ${n1 * n2}`);
+console.log(`a divisão destes números é ${n1/n2}`);
---
+++ ex5.js
@@ -0,0 +1,8 @@
+var num1 = 1;
+var num2 = 2;
+
+function soma(num1, num2){
+    return num1 + num2;
+}
+
+console.log(soma(1578, 909));
